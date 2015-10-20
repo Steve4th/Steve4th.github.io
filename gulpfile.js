b3bc1113936dbcf02d3122ca8a4d5256375f1c09
Steve 4th <steveforth@hotmail.com>
2015-10-20 09:11:38 +0100

    Added JS to control the nav menu behaviour

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
+var jsmin = require('gulp-uglify');
 
 gulp.task('less', function() {
     gulp.src('./less/*.less')
@@ -12,5 +13,13 @@ gulp.task('less', function() {
 });
 
 gulp.task('default', function() {
-    gulp.watch('*.less', ['less']);
+    gulp.watch('./less/*.less', ['less']);
+    gulp.watch('./js/site.js', ['js']);
 })
+
+gulp.task("js", function() {
+   gulp.src('./js/site.js')
+        .pipe(concat('site.min.js'))
+        .pipe(jsmin())
+        .pipe(gulp.dest('./js/')); 
+});
